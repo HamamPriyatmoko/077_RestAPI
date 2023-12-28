4c9720a2314d51e09599e5cd6a41f9cbcc275e90
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2023-12-28 21:13:27 +0700

    menambahkan parameter

--- app/src/main/java/com/example/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -1,2 +1,8 @@
 package com.example.consumeapi.ui.kontak.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.example.consumeapi.repository.KontakRepository
+
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel(){
+}
