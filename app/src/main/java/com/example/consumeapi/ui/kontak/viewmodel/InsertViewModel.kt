9d5717df1cf15cc648b5b66817449986ebc16853
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2023-12-28 21:24:08 +0700

    menambahkan fun kontak.touievent

--- app/src/main/java/com/example/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -28,3 +28,11 @@ fun InsertUiEvent.toKontak() : Kontak = Kontak(
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
 )
+
+
+fun Kontak.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    nohp = nohp
+)
