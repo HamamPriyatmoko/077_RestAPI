0766907ec303f2e6294ca1fa974249ce414de384
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-01-05 17:30:19 +0700

    php connect

--- app/src/main/java/com/example/consumeapi/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/consumeapi/ui/PenyediaViewModel.kt
@@ -1,11 +1,14 @@
 package com.example.consumeapi.ui
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.consumeapi.KontakAplikation
 import com.example.consumeapi.ui.home.viewmodel.HomeViewModel
+import com.example.consumeapi.ui.kontak.viewmodel.DetailsViewModel
+import com.example.consumeapi.ui.kontak.viewmodel.EditViewModel
 import com.example.consumeapi.ui.kontak.viewmodel.InsertViewModel
 
 object PenyediaViewModel {
@@ -15,6 +18,18 @@ object PenyediaViewModel {
         }
         initializer {
             InsertViewModel(aplikasiKontak().container.KontakRepository)
+        }
+        initializer {
+            DetailsViewModel(
+                createSavedStateHandle(),
+                kontakRepository = aplikasiKontak().container.KontakRepository
+            )
+        }
+        initializer {
+            EditViewModel(
+                createSavedStateHandle(),
+                kontakRepository = aplikasiKontak().container.KontakRepository
+            )
         }
     }
 }
---
+++ app/src/main/java/com/example/consumeapi/ui/kontak/viewmodel/DetailsViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.consumeapi.ui.kontak.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.consumeapi.model.Kontak
+import com.example.consumeapi.repository.KontakRepository
+import com.example.consumeapi.ui.kontak.screen.DetailsDestination
+import kotlinx.coroutines.launch
+
+sealed class DetailsKontakUiState {
+
+    data class Success(
+        val kontak: Kontak) : DetailsKontakUiState()
+
+    object Error : DetailsKontakUiState()
+
+    object Loading : DetailsKontakUiState()
+}
+
+class DetailsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val kontakRepository: KontakRepository
+) : ViewModel() {
+    private val kontakId: Int = checkNotNull(savedStateHandle[DetailsDestination.kontakId])
+
+    var detailsKontakUiState: DetailsKontakUiState by mutableStateOf(DetailsKontakUiState.Loading)
+        private set
+
+    init {
+        getKontakById()
+    }
+    fun getKontakById(){
+        viewModelScope.launch {
+            detailsKontakUiState = DetailsKontakUiState.Loading
+            detailsKontakUiState = try {
+                DetailsKontakUiState.Success(
+                    kontak = kontakRepository.getkontakById(kontakId)
+                )
+            }catch (e: Exception){
+                DetailsKontakUiState.Error
+            }
+        }
+    }
+}
